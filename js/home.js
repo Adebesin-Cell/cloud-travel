c2ebe6b96a4bae637bff173eb05861afc797a4ce
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-03-18 22:54:13 +0100

    updating UI

--- js/home.js
+++ js/home.js
@@ -59,3 +59,55 @@ btnScrollTo.forEach((btn) => {
     header.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const searchBtn = document.querySelector(".search");
+const searchMenu = document.querySelector(".menu");
+const searchMenuCloseBtn = document.querySelector(".menu__icon--close");
+const body = document.querySelector("body");
+
+searchBtn.addEventListener("click", function (e) {
+  searchMenu.classList.remove("menu--hidden");
+  body.classList.add("no-scroll");
+});
+
+searchMenuCloseBtn.addEventListener("click", function (e) {
+  searchMenu.classList.add("menu--hidden");
+  body.classList.remove("no-scroll");
+});
+
+const filterBtn = document.querySelector(".filter__button--open");
+const filterMenu = document.querySelector(".filter-menu");
+const filterMenuCloseBtn = document.querySelector(".filter-menu__icon--close");
+
+filterBtn.addEventListener("click", function (e) {
+  filterMenu.classList.remove("filter-menu--hidden");
+  body.classList.add("no-scroll");
+});
+
+filterMenuCloseBtn.addEventListener("click", function (e) {
+  filterMenu.classList.add("filter-menu--hidden");
+  body.classList.remove("no-scroll");
+});
+
+const selectField = document.querySelector(".select__field");
+const selectInput = document.querySelector(".select__input");
+const options = document.querySelectorAll(".select__item");
+const selectList = document.querySelector(".select__list");
+const selectIcon = document.querySelector(".select__field .icon svg");
+
+selectInput.blur();
+
+selectField.addEventListener("click", function () {
+  selectList.classList.toggle("hidden");
+  selectIcon.classList.toggle("rotate");
+  selectInput.blur();
+});
+
+options.forEach((option) => {
+  option.addEventListener("click", function (e) {
+    selectInput.value = this.textContent;
+    selectList.classList.toggle("hidden");
+    selectIcon.classList.toggle("rotate");
+    selectInput.blur();
+  });
+});
